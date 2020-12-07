1c1a087fe49feb921a22a1b283e3174962c5b725
Stephen Harrison <stephen@stephen.cloud>
2020-12-07 18:40:27 -0500

    Fix token

--- build-image/start.sh
+++ build-image/start.sh
@@ -11,11 +11,13 @@ token=$(curl -s -XPOST \
     -H "authorization: token ${PAT}" \
     https://api.github.com/repos/stephen-cloud/cdk-github-actions/actions/runners/registration-token |\
     jq -r .token)
+
+echo token $token
 #token=$(curl -s -XPOST \
 #    -H "authorization: token ${PAT}" \
 #    https://api.github.com/orgs/nogbadthebad/actions/runners/registration-token |\
 #    jq -r .token)
 
-./config.sh --url https://github.com/stephen-cloud --token $token
+./config.sh --url https://github.com/stephen-cloud/cdk-github-actions --token $token
 # ./config.sh --url $URL --token $PAT --name "aws-runner-$(hostname)" --work _work
 ./run.sh
